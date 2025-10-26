3229301ad85fff53c82cd08f3e4fddc6dc78a648
AkashK0907 <akashk79026@gmail.com>
2025-10-26 19:18:02 +0530

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { register, login, verifyOTP, completeSetup } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
+router.post('/register', register);
+router.post('/login', login);
+router.post('/verify-otp', verifyOTP);
+router.put('/complete-setup', protect, completeSetup);
+
+module.exports = router;
 
